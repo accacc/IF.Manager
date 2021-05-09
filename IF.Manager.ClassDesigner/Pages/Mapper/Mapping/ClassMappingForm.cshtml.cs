8d881b02e45aa24faea00146cdc4e7b36d799182
accacc <accacc@gmail.com>
2021-05-09 21:56:21 +0300

    chage dropdowns old style

--- IF.Manager.ClassDesigner/Pages/Mapper/Mapping/ClassMappingForm.cshtml.cs
+++ IF.Manager.ClassDesigner/Pages/Mapper/Mapping/ClassMappingForm.cshtml.cs
@@ -1,3 +1,4 @@
+using IF.Core.Algorithms;
 using IF.Manager.Contracts.Dto;
 using IF.Manager.Contracts.Services;
 using IF.Manager.Service.Model;
@@ -46,7 +47,14 @@ namespace IF.Manager.ClassDesigner.Pages.Mapper.Mapping
                 SetEmptyForm();
             }
 
-            await SetFormDefaults();
+            await SetFormDefaults(false);
+
+        }
+
+        public async Task OnGetAutoMapping()
+        {
+            this.Form = new List<IFClassMapping>();
+            await SetFormDefaults(true);
 
         }
 
@@ -72,10 +80,8 @@ namespace IF.Manager.ClassDesigner.Pages.Mapper.Mapping
 
         }
 
-        private async Task SetFormDefaults()
+        private async Task SetFormDefaults(bool IsAutoFill = false)
         {
-
-
             var mapping = await this.classService.GetClassMapper(this.ClassMapId);
 
             List<IFClass> classes = await this.classService.GetClassFlattenList(mapping.IFClassId.Value);
@@ -86,15 +92,52 @@ namespace IF.Manager.ClassDesigner.Pages.Mapper.Mapping
 
             SetModels(models);
 
+            if (!IsAutoFill) return;
+
+            foreach (var model in models)
+            {
+                List<LevenshteinDistanceResult> levenshteinDistanceResultList = new List<LevenshteinDistanceResult>();
+
+                foreach (var @class in classes)
+                {
+                    if (!@class.IsPrimitive) continue;
+
+                    LevenshteinDistanceResult levenshteinDistanceResult = new LevenshteinDistanceResult();
+
+                    int score = LevenshteinDistance.Calculate(model.Name, @class.Name);
+
+
+                    levenshteinDistanceResult.score = score;
+                    levenshteinDistanceResult.string2 = model.Name;
+                    levenshteinDistanceResult.string1 = @class.Name;
+                    levenshteinDistanceResult.id1 = @class.Id;
+                    levenshteinDistanceResult.id2 = model.ModelPropertyId;
+
+                    levenshteinDistanceResultList.Add(levenshteinDistanceResult);
+                }
+
+                var min = levenshteinDistanceResultList.Min(l => l.score);
+
+                if (min <= 3)
+                {
+
+                    var bestMatch = levenshteinDistanceResultList.Where(r => r.score == min).FirstOrDefault();
+
+                    this.Form.Add(new IFClassMapping { FromPropertyId = bestMatch.id1, ToPropertyId = bestMatch.id2 });
+                }
+            }
+
+
+
         }
 
         private void SetModels(List<ModelPropertyDto> models)
         {
-            List<SelectListItem> items = new List<SelectListItem>();
+            List<DropDownClass> items = new List<DropDownClass>();
 
             foreach (var property in models)
             {
-                SelectListItem item = new SelectListItem();
+                DropDownClass item = new DropDownClass();
 
                 item.Text = property.EntityName + " - " + property.Name;
                 item.Value = property.ModelPropertyId.ToString();
@@ -121,21 +164,12 @@ namespace IF.Manager.ClassDesigner.Pages.Mapper.Mapping
         {
 
 
-            List<SelectListItem> items = new List<SelectListItem>();
+            List<DropDownClass> items = new List<DropDownClass>();
 
 
             foreach (var @class in classes)
             {
-                SelectListItem item = new SelectListItem();
-
-                //if (@class.Parent == null)
-                //{
-                //    item.Text = @class.Name;
-                //}
-                //else
-                //{
-                //    item.Text = @class.Parent.Name + " - " + @class.Name;
-                //}
+                DropDownClass item = new DropDownClass();
 
                 item.Text = @class.GetRootPathWithoutRoot() + "." + @class.Name;
                 item.Value = @class.Id.ToString();
@@ -146,5 +180,28 @@ namespace IF.Manager.ClassDesigner.Pages.Mapper.Mapping
 
             ViewData["classes"] = items;
         }
+    }
+
+    public class DropDownClass
+    {
+
+        public string Text { get; set; }
+        public string Value { get; set; }
+
+        public bool Selected { get; set; }
+
+        public bool Choosen { get; set; }
+    }
+
+    public class LevenshteinDistanceResult
+    {
+        public string string1 { get; set; }
+        public string string2 { get; set; }
+
+        public int id1 { get; set; }
+
+        public int id2 { get; set; }
+
+        public int score { get; set; }
     }
 }
